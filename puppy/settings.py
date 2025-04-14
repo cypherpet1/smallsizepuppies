71e8555fecaea410a325377eaaae5ab81dddfb94
cypherpet1 <smallbreedpuppies79@gmail.com>
2025-04-14 15:15:41 +0100

    Requirements  File

--- puppy/settings.py
+++ puppy/settings.py
@@ -132,3 +132,7 @@ import os
 # Media settings for user-uploaded content
 MEDIA_URL = '/media/'  # URL path for accessing media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+<<<<<<< HEAD
+=======
+
+>>>>>>> dabae14 (Requirements  File)
